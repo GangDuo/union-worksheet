b5453ccd109b1ef11e91870d07e11d7a15e4a848
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-05-13 17:52:53 +0900

    ファイルdrop後、ファイル読み込み完了するまで画面入力を一切受け付けない。

--- src/components/FileUploadForm.js
+++ src/components/FileUploadForm.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import { Container, Button, LinearProgress } from '@material-ui/core';
 import {DropzoneArea} from 'material-ui-dropzone'
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
 
 function FileUploadForm({ onSubmit }) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
   return (
     <Formik
       initialValues={{}}      
@@ -12,7 +25,7 @@ function FileUploadForm({ onSubmit }) {
       {({ submitForm, isSubmitting, setFieldValue }) => (
         <Container maxWidth="sm">
         <Form>
-
+          
           <fieldset>
             <legend>Excelファイル</legend>
 
@@ -26,9 +39,13 @@ function FileUploadForm({ onSubmit }) {
                    maxFileSize={10000000/* 10Mbytes */}
                    filesLimit={99}
                    showFileNames={true}
-                   onDrop={droppedFiles => /*loading 表示*/console.log(droppedFiles)}
+                   onDrop={droppedFiles => {
+                     setOpen(true);
+                     console.log(droppedFiles)
+                   }}
                    onChange={loadedFiles => {
-                     console.log('DropzoneArea.onChange')
+                    setOpen(false);
+                    console.log('DropzoneArea.onChange')
                      if(loadedFiles.length === 0) return;
                      setFieldValue('loadedFiles', loadedFiles.map(x=>URL.createObjectURL(x)))
                    }} />
@@ -46,6 +63,9 @@ function FileUploadForm({ onSubmit }) {
             結合
           </Button>
         </Form>
+        <Backdrop className={classes.backdrop} open={open} >
+          <CircularProgress color="inherit" />
+        </Backdrop>
         </Container>
       )}
     </Formik>
