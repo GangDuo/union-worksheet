d8cc960d93c6bc6c07e1cd34da3d75ddb8218a0a
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-05-14 11:40:23 +0900

    結合ボタンをクリックし、エクセルファイルをダウンロードさせる。

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import FileUploadForm from './components/FileUploadForm.js'
+const XLSX = require('xlsx')
 
 function App() {
   return (
     <FileUploadForm onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        setSubmitting(false);
-        alert(JSON.stringify(values, null, 2));
-      }, 500);
+      new Promise((resolve, reject) => {
+        setImmediate(_ => {
+          const workbook = XLSX.utils.book_new()
+          const sheet = XLSX.utils.json_to_sheet([{a:1,b:2}])
+          // すべてのセルの書式設定を文字列にする
+          Object.keys(sheet).map(x => sheet[x] = Object.assign(sheet[x], {'z': '@'}))
+          XLSX.utils.book_append_sheet(workbook, sheet)
+          resolve(XLSX.writeFile(workbook, "filename.xlsx"));
+          setSubmitting(false);
+          console.log(JSON.stringify(values, null, 2));
+        });
+      })
+      .then(file => file);
     }} />
   );
 }
