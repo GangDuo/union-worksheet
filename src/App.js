766b446c00347d18468cfb2846b066bad1e66815
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-05-19 11:25:07 +0900

    複数エクセルの1番目のシートを新しいエクセルの1番目のシートに結合

--- src/App.js
+++ src/App.js
@@ -2,22 +2,72 @@ import React from 'react';
 import FileUploadForm from './components/FileUploadForm.js'
 const XLSX = require('xlsx')
 
+function saveAs(data, filename) {
+  const workbook = XLSX.utils.book_new()
+  const sheet = XLSX.utils.json_to_sheet(data)
+   
+  // すべてのセルの書式設定を文字列にする
+  Object.keys(sheet).map(x => sheet[x] = Object.assign(sheet[x], {'z': '@'}))
+  XLSX.utils.book_append_sheet(workbook, sheet)
+  return XLSX.writeFile(workbook, filename)
+}
+
+function download(url, callback) {
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.responseType = "arraybuffer";
+  
+  req.onerror = () => {
+    callback(new Error("** An error occurred during the transaction"))
+  };
+  req.onload = (e) => {
+    console.log(url);
+    callback(null, req.response)
+  }
+  
+  req.send();
+}
+
+function readXlsxAsJson(buf) {
+  var data = new Uint8Array(buf);
+  var workbook = XLSX.read(data, {type:"array"});
+
+  /* DO SOMETHING WITH workbook HERE */
+  const sheet1 = workbook.Sheets[workbook.SheetNames[0]]
+  return XLSX.utils.sheet_to_json(sheet1)
+}
+
+function makeJob(values) {
+  return values.loadedFiles.map(file => {
+    return new Promise((resolve, reject) => {
+      download(file, (err, response) => {
+        if(err) {
+          reject(err)
+          return
+        }
+        const sheet1AsJson = readXlsxAsJson(response)
+        console.log(sheet1AsJson);
+        resolve(sheet1AsJson)
+      })
+    })
+  })
+}
+
 function App() {
   return (
     <FileUploadForm onSubmit={(values, { setSubmitting }) => {
-      new Promise((resolve, reject) => {
+      console.log(JSON.stringify(values, null, 2));
+      return new Promise((resolve, reject) => {
         setImmediate(_ => {
-          const workbook = XLSX.utils.book_new()
-          const sheet = XLSX.utils.json_to_sheet([{a:1,b:2}])
-          // すべてのセルの書式設定を文字列にする
-          Object.keys(sheet).map(x => sheet[x] = Object.assign(sheet[x], {'z': '@'}))
-          XLSX.utils.book_append_sheet(workbook, sheet)
-          resolve(XLSX.writeFile(workbook, "filename.xlsx"));
-          setSubmitting(false);
-          console.log(JSON.stringify(values, null, 2));
-        });
+          resolve(makeJob(values))
+        })
       })
-      .then(file => file);
+      .then(jobs => Promise.all(jobs))
+      .then((data) => {
+        const file = saveAs(Array.prototype.concat.apply([], data), "filename.xlsx")
+        setSubmitting(false);
+        return Promise.resolve(file)
+      })
     }} />
   );
 }
